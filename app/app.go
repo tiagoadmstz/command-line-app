ce5da83b23ba3cf74cc1806b85b852defa98664f
tiagoadmstz <tiagoadmstz@yahoo.com.br>
2022-07-27 19:38:09 -0300

    created server search command

--- app/app.go
+++ app/app.go
@@ -18,17 +18,25 @@ func Generate() *cli.App {
 }
 
 func configure(app *cli.App) {
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search for IP adresses on the internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Search for IP adresses on the internet",
+			Flags:  flags,
 			Action: searchIp,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Search for servers names on the internet",
+			Flags:  flags,
+			Action: searchServers,
 		},
 	}
 }
@@ -42,5 +50,17 @@ func searchIp(context *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func searchServers(context *cli.Context) {
+	host := context.String("host")
+	servers, err := net.LookupNS(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
